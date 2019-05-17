66fd2b17bafd13642e2335129e565e340e744180
leslieterrero <49455634+leslieterrero@users.noreply.github.com>
2019-05-17 12:45:44 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class= "container">
+   <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+   <app-many-quotes :quotes="quotes" @quoteDeleted="eraseQuote"> </app-many-quotes>
+   <app-new-quote @quoteAdded="addNewQ"> </app-new-quote>
+
+   <div class="row">
+      <div class="col-sm-12 text-center">
+      <div class="alert alert-info">Info: Click on a quote to delete it (:</div>
+      </div>
+
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+import ManyQuotes from './components/ManyQuotes.vue';
+import NewQuote from './components/NewQuote.vue';
+import Header from './components/Header.vue';
+
+
+export default {
+
+  data: function () {
+    return {
+
+      quotes: [
+      
+      'Good things take time'
+
+      ],
+
+      maxQuotes: 10
+    }
+  },
+
+  methods: {
+   addNewQ(quote) {
+                if (this.quotes.length >= this.maxQuotes) {
+                    return alert('Please delete some Quotes first!');
+                }
+                this.quotes.push(quote);
+            },
+
+
+    eraseQuote(index){
+      this.quotes.splice(index, 1);
+    }
+  },
+
+ components: {
+
+
+   appManyQuotes: ManyQuotes,
+   appNewQuote: NewQuote,
+   appHeader: Header
+  
+ }
+
+}
+</script>
+
+<style>
+
+
+</style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,35 @@
+<template> 
+
+<div class="col-sm-6 col-md-4 col-lg-3">
+<div class="panel panel-default">
+<div class="panel-body quote">
+<slot> </slot>
+
+</div>
+   </div>
+     </div>
+
+
+</template>
+
+
+<script>
+
+</script>
+
+<style>
+.panel-body{
+     font-family: 'Times New Roman', cursive;
+     font-size: 20px;
+     color: MediumVioletRed ;
+     
+}
+
+.quote{
+cursor: pointer;
+}
+
+.quote:hover{
+background-color: Thistle  ;
+}
+</style>
